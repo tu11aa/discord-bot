3bb0dce2ef330aac1be71f813189444ee2aa217f
tu11aa <thanhtuhr01@gmail.com>
2022-12-12 20:11:00 +0700

    done v1.0.0

---
+++ commands/tu_oi.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require("discord.js");
+require("dotenv").config();
+const wait = require("node:timers/promises").setTimeout;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("tu_oi")
+    .setDescription("Nhớ Tú thì kêu Tú điii")
+    .addSubcommand((subcommand) =>
+      subcommand.setName("hi").setDescription("Say Hi")
+    ),
+  // .addSubcommand((subcommand) =>
+  //   subcommand.setName("play_yt").setDescription("Play Youtube music")
+  // ),
+  async execute(interaction) {
+    const subCommand = interaction.options.getSubcommand();
+    if (subCommand === "hi") await defalultFunc(interaction);
+    else if (subCommand === "play_yt") await play_ytFunc(interaction);
+  },
+};
+
+const defalultFunc = async (interaction) => {
+  await interaction.editReply("Hello bé êu của Túuuu");
+  const sender = interaction.member;
+  if (sender.id === process.env.QUIN_ID) {
+    await wait(500);
+    await interaction.editReply(
+      `Úi bé <@${sender.id}> hở, iu bé nhiềuuuuuu!!!`
+    );
+    await wait(500);
+    await interaction.followUp(
+      "Chị là t2 hông ai chủ nhật. Chị Quìn mãi đỉnhhhhh♥♥♥"
+    );
+  } else if (sender.id === process.env.ME_ID) {
+    await wait(500);
+    await interaction.editReply(`Úi BOSS <@${sender.id}> hở!!!`);
+    await wait(500);
+    await interaction.followUp("Đại ca muôn năm♥♥♥");
+  }
+};
+
+const play_ytFunc = async (interaction) => {
+  // await interaction.reply("Playing YT");
+  await interaction.deferReply();
+  if (!interaction.member.voice?.channel)
+    return interaction.editReply("Connect to a Voice Channel");
+};
---
+++ index.js
@@ -0,0 +1,81 @@
+const { GatewayIntentBits, Events, Collection } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const cron = require("cron");
+const wait = require("node:timers/promises").setTimeout;
+
+require("dotenv").config();
+const Client = require("discord.js").Client;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences,
+  ],
+});
+//settup splash commands
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  const task = async () => {
+    const guild = client.guilds.cache.get(process.env.SERVER_ID);
+    guild.members
+      .fetch({ withPresences: true })
+      .then(async (fetchedMembers) => {
+        const totalOnline = fetchedMembers.filter(
+          (member) => member.presence?.status === "online"
+        );
+        // Now you have a collection with all online member objects in the totalOnline variable
+        totalOnline.forEach(async (member) => {
+          await member.send("Éc quá giờ gòi đi ngủ mauuuuuuuu");
+        });
+        await wait(1000);
+        totalOnline.forEach(async (member) => {
+          await member.send("Ngủ đê bé ơi đêm đã khuya gòiiii");
+        });
+        await wait(1000);
+        totalOnline.forEach(async (member) => {
+          await member.send("Nhớ đi ngủ đấyyyy");
+        });
+      });
+  };
+  const schedule1 = new cron.CronJob("00 00 01 * * *", task);
+  const schedule2 = new cron.CronJob("00 30 01 * * *", task);
+  schedule1.start();
+  schedule2.start();
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
